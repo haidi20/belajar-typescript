ebd18c7cfbeb2abac69ead6f0e5a938783bfb527
Alief Kresna <p1H12030@pamapersada.net>
2020-08-31 08:47:25 +0800

    hello world

---
+++ ts-src/app.ts
@@ -0,0 +1,5 @@
+import * as $ from 'jquery';
+
+$(function () {
+  $('#message').html('hello world');
+});
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var path = require('path');
+
+module.exports = {
+    entry: './ts-src/app.ts',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.js']
+    },
+    module: {
+        rules: [{
+            test: /.ts$/,
+            loader: 'ts-loader'
+        }]
+    },
+    output: {
+        filename: 'app.js',
+        path: path.resolve(__dirname, 'build')
+    }
+};
