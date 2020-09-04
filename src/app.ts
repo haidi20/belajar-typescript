5fca3d9135c3737c480504a7bd43e28fdee0b706
haidi20 <nhaidii@yahoo.com>
2020-09-04 17:43:04 +0800

    make variable string, number, and array

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,21 @@
 import * as $ from 'jquery';
 
-$(function () {
-  $('#message').html('hello world');
+let name: string = "My Name is Haidi.";
+let age: number = 23;
+let ListFriends: string = '';
+let myFriends:[string, number] []= [
+  ["Adhi", 24],
+  ["Andi", 20],
+  ["Risky", 22],
+];
+
+$(document).ready(function () {
+  $('#name').html(name);
+  $('#age').html(`My age is ${age} Years old.`);
+
+  myFriends.map(item => {
+    ListFriends += `<li>Name : ${item[0]} and Age : ${item[1]} </li>`;
+  });
+
+  $('#myFriends').html(ListFriends);
 });
