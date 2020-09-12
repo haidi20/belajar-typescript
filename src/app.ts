b598cff24254db7f710d33cff5682cc2efb8ae65
haidi20 <nhaidii@yahoo.com>
2020-09-12 17:36:47 +0800

    tutorial condition

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,11 @@
 import * as $ from 'jquery';
 
-let name: string = "My Name is Haidi.";
-let age: number = 23;
-let ListFriends: string = '';
-let myFriends:[string, number] []= [
-  ["Adhi", 24],
-  ["Andi", 20],
-  ["Risky", 22],
-];
+let age: number = 26;
 
 $(document).ready(function () {
-  $('#name').html(name);
-  $('#age').html(`My age is ${age} Years old.`);
-
-  myFriends.map(item => {
-    ListFriends += `<li>Name : ${item[0]} and Age : ${item[1]} </li>`;
-  });
-
-  $('#myFriends').html(ListFriends);
+  if (age <= 25) {
+    $('#result').html('still young')
+  } else {
+    $('#result').html('old');
+  }
 });
